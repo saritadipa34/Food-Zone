0c43da2c2412a3522fcc10661cfda1662aaddc97
saritadipa34 <saritadipa34@gmail.com>
2024-11-24 21:01:56 +0530

    WIP: Partial TopSection UI completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,18 @@ function App() {
   return (
    <Container>
     <TopSection>
-      <img src="/images/logo.jpg" alt="logo" />
-      </TopSection>
+      <Img src="/images/logo.png" alt="logo" />
+      
+      <Lists>
+          <Button>All</Button>
+          <Button>Breakfast</Button>
+          <Button>Lunch</Button>
+          <Button>Dinner</Button>
+      </Lists>
       <Search>
-        <input type="text" placeholder='Search Food...' />
+        <Input type="text" placeholder='Search Food...' />
       </Search>
+      </TopSection>
    </Container>
   )
 }
@@ -18,14 +25,46 @@ function App() {
 export default App
 
 const Container=styled.div({
-  backgroundColor:'green',
- height:'100%',
+  backgroundColor:'rgb(255, 204, 153)',
+minHeight:'100vh',
  width:'100%',
+
 })
 
 const TopSection=styled.section({
-width:'100%',
-height:'100px',
-backgroundColor:'pink',
+  width:'100%',
+  height:'100px',
+  display:'flex',
+  justifyContent:'space-between',
+  backgroundColor:'rgb(255, 179, 102)',
+  })
 
+const Img =styled.img({
+width:'200px',
+height:'100px',
+mixBlendMode:'multiply',
 })
+
+const Search =styled.div({
+ 
+  })
+
+  const Input=styled.input({
+    margin:'35px',
+    padding:'5px',
+    backgroundColor:'rgb(255, 204, 153)',
+    borderRadius:'5px',
+  })
+
+  const Lists=styled.div({
+    display:'flex',
+    margin:'70px',
+  })
+
+  const Button=styled.button({
+backgroundColor:'rgb(255, 204, 102)',
+padding:'2px',
+height:'20px',
+borderRadius:'5px',
+margin:'5px',
+  })
