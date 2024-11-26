f8f1a666424fe475a63b7ab1b71e478a6e8825cc
saritadipa34 <saritadipa34@gmail.com>
2024-11-26 14:08:19 +0530

    Search functionality implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import styled from 'styled-components';
-import './App.css'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data,setData]=useState([]);
+  const[filteredData,setFilteredData]=useState(null)
+  
+const DATA_URL='http://localhost:9000/'
+
+const inputChange=(e)=>{
+  const searchValue=e.target.value;
+  console.log(searchValue);
+
+ const filtered=data.filter((food)=>food.name.toLowerCase().includes(searchValue.toLowerCase()));
+ console.log(filtered);
+ setFilteredData(filtered);
+  };
+
+
+useEffect(()=>{
+  const fetchData=async()=>{
+try{
+  const res=await fetch(DATA_URL)
+const json= await res.json();
+setData(json);
+// setFilteredData(json);
+console.log(json);
+
+}
+catch(error){
+  console.error(error)
+}
+  }
+  fetchData()
+},[])
 
   return (
    <Container>
@@ -9,26 +40,43 @@ function App() {
       <Img src="/images/logo.png" alt="logo" />
       
       <Lists>
-          <Button>All</Button>
-          <Button>Breakfast</Button>
-          <Button>Lunch</Button>
-          <Button>Dinner</Button>
+          <Button onClick ={()=>handleBtn('All')}>All</Button>
+          <Button onClick ={()=>handleBtn('Breakfast')}>Breakfast</Button>
+          <Button onClick ={()=>handleBtn('Lunch')}>Lunch</Button>
+          <Button onClick ={()=>handleBtn('Dinner')}>Dinner</Button>
       </Lists>
+
       <Search>
-        <Input type="text" placeholder='Search Food...' />
+        <Input type="text" placeholder='Search Food...'
+        onChange={inputChange}
+        />
       </Search>
       </TopSection>
+
+<CardContainer>
+{(filteredData||data).map((item,i)=>(
+   <Card key={i}> 
+    <div>
+    <img src={item.image} alt={item.name} />
+    </div>
+    <div>
+<h4>{item.name}</h4>
+    <Des>{item.text}</Des>
+    </div>
+  </Card>
+))}
+</CardContainer>
+
    </Container>
-  )
+  ) 
 }
 
 export default App
 
 const Container=styled.div({
-  backgroundColor:'rgb(255, 204, 153)',
+backgroundColor:'rgb(255, 204, 153)',
 minHeight:'100vh',
- width:'100%',
-
+width:'100%',
 })
 
 const TopSection=styled.section({
@@ -45,9 +93,7 @@ height:'100px',
 mixBlendMode:'multiply',
 })
 
-const Search =styled.div({
- 
-  })
+const Search =styled.div``
 
   const Input=styled.input({
     margin:'35px',
@@ -61,10 +107,41 @@ const Search =styled.div({
     margin:'70px',
   })
 
-  const Button=styled.button({
+const Button=styled.button({
 backgroundColor:'rgb(255, 204, 102)',
-padding:'2px',
+padding:'2px 5px',
 height:'20px',
 borderRadius:'5px',
 margin:'5px',
+cursor:'pointer',
   })
+
+const CardContainer=styled.div({
+height:'80vh',
+width:'85vw',
+display:'flex',
+flexWrap:'wrap',
+opacity:'90%',
+margin:'0 100px',
+gap:'20px',
+padding:'20px 40px',
+backgroundImage:'url("/images/bg.jpg")',
+})
+
+const Card=styled.div({
+height:'160px',
+width:'320px',
+borderRadius:'10px',
+backgroundColor:'rgb(255, 204, 153)',
+// opacity:'70%',
+padding:'10px ',
+display:'flex',
+cursor:'pointer',
+border:'1px solid white',
+})
+
+const Des=styled.p`
+margin:10px;
+
+`
+const SearchResult=styled.div``
